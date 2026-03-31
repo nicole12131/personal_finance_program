ff1fe3fbe806533e9871dff3ceffd6b4d97882aa
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-03-31 09:41:08 -0600

    finish data management pseudocode

---
+++ Data_management.py
@@ -0,0 +1,36 @@
+# import csv
+
+# Load data from the saving goal CSV file
+
+# create function for new saving goal
+
+# Ask user if they already have a saving goal
+
+    # if user say no
+        # ask for a new saving goal
+
+    # if user say yes 
+        # Input current savings
+
+    # else
+        # print invalid choice  
+
+# create function for saving progress
+
+# calcualte progress towards the goal
+
+# if user is doing a good progress 
+    # print that they are doing a good progress
+
+# if user is doing a bad progress
+    # print options to improve savings to complete the goal
+
+# Save data to the saving goal CSV file
+# display final progress 
+
+# create function to save progress between sessions 
+# if user if loged in print their current savings
+# if user want to log out save their progress to the saving goal CSV file
+
+# if a new profile is created add it to the csv file
+    # if user make progress add it to csv file
