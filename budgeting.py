1baead6bae38bb8dadd664e4b372bbdc35b0d170
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-04-01 13:45:05 -0600

    pseudocode for budgeting and main done

--- budgeting.py
+++ budgeting.py
@@ -1,1 +1,11 @@
+# MH 1st Budgeting Functions
 
+# Set limits function:
+    # For every budget category ask how much the user is willing to spend
+    # updates each value in the list pulled from the csv
+    # returns the updated list
+
+# Compare expenses function:
+    # takes from the list how much the user has spent in each category
+    # subtracts what they've spent in each category from what the limit is in each category
+    # prints out how much they have left to spend for each category
---
+++ main.py
@@ -0,0 +1,40 @@
+# MH 1st Main file
+
+# imports
+
+# load csv as list
+
+# main function:
+    # print a welcome and instructions for the user
+    # WHILE loop:
+        # ask if the user wants to log in, create an account, or quit
+        # LOG IN:
+            # run the log in function
+            # set account status to active
+            # provide user with more options: add expense, add income, view total income or expenses in a specified time, set budget limits, compare budget to expenses, set savings goal, track progress towards savings goal, view budget as pie chart, view income/expenses as graph, log out
+            # ADD EXPENSE:
+                # run the add expense function
+            # ADD INCOME:
+                # run the add income function
+            # VIEW INCOME/EXPENSES IN A SPECIFIED TIME:
+                # run the function to view income/expenses in a specified time
+            # SET LIMITS:
+                # run the set budget limits function
+            # COMPARE BUDGET/EXPENSES:
+                # run the compare budget and expenses function
+            # SET SAVINGS GOAL:
+                # run the set savings goal function
+            # TRACK PROGRESS TO GOAL:
+                # run the track progress to savings goal function
+            # VIEW PIE CHART:
+                # run the make pie chart function
+            # VIEW GRAPH:
+                # run the make graph function
+            # LOG OUT:
+                # set account status to inactive
+                # take the user back to the first selection of choices
+        # CREATE ACCOUNT:
+            # run the create account function
+            # take the user back to the first selection of choices
+        # QUIT:
+            # exit the while loop
