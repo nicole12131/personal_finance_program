908d29cc30d57e87052dcef9ae93f735e17a0524
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-04-07 09:51:16 -0600

    data management

--- Data_management.py
+++ Data_management.py
@@ -5,23 +5,16 @@ import csv
 # create function for new saving goal
 def new_goal():
 # Ask user if they already have a saving goal
-    goal = input("Do you have a saving goal?: ")
     # if user say no
-    if goal == "no":
         # ask for a new saving goal
-        new = input("Type a new saving goal: ")
     # if user say yes
-    if goal == "yes":
         # Input current savings
-        print("current savings")
     # else
-    else:
         # print invalid choice 
-        print("invalid choice try again")
-        new_goal()
+        
 
 # create function for saving progress
-#def saving_progress():
+def saving_progress():
 # calcualte progress towards the goal
 
 # if user is doing a good progress 
@@ -34,10 +27,9 @@ def new_goal():
 # display final progress 
 
 # create function to save progress between sessions 
-#def save_progress():
+def progress_sessions():
 # if user if loged in print their current savings
 # if user want to log out save their progress to the saving goal CSV file
 
 # if a new profile is created add it to the csv file
     # if user make progress add it to csv file
-new_goal()
