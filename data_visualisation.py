361daece1fb49381b6f1b2c2155babc1d5b7d124
Queverity <pryor.baird@ucas-edu.net>
2026-04-08 16:20:21 -0600

    fixed problems

--- data_visualisation.py
+++ data_visualisation.py
@@ -3,7 +3,7 @@
 # import numpy, maybe pandas
 import matplotlib.pyplot as plt
 import customtkinter as ctk
-from budgeting import *
+import csv
 
 
 # define function make pie charts (come up with a better name)
@@ -19,6 +19,52 @@ from budgeting import *
     # take info from user CSV, and order based off of input date
     # plug those numbers into pandas to generate a pretty-ish chart, return and print on screen
 
+def get_expense_data(expenses_path):
+    with open(expenses_path, "r") as expenses_csv:
+        # loops over csv and converts lines into dictionaries
+        content = csv.reader(expenses_csv)
+        row_count = sum(1 for row in content)
+        expenses_csv.seek(0)
+        if row_count == 0:
+            headers = ["date", "location", "amount", "category"]
+        else:
+            headers = next(content)
+        rows = []
+        for line in content:
+            rows.append({headers[0] : line[0], headers[1] : line[1], headers[2] : line[2], headers[3] : line[3]})
+        return rows
+        
+def get_amounts(expenses_path):
+    food = 0
+    rent = 0
+    utilities = 0
+    transportation = 0
+    entertainment = 0
+    # get expenses data from the users expense csv
+    expense_csv = get_expense_data(expenses_path)
+    # check expense category and add it to the corresponding total
+    for expense in expense_csv:
+        if expense["category"] == "food":
+            food += float(expense["amount"])
+        elif expense["category"] == "rent":
+            rent += float(expense["amount"])
+        elif expense["category"] == "utilities":
+            utilities += float(expense["amount"])
+        elif expense["category"] == "transportation":
+            transportation += float(expense["amount"])
+        elif expense["category"] == "entertainment":
+            entertainment += float(expense["amount"])
+    return food, rent, utilities, transportation, entertainment
+
+def get_budget(budget_path):
+    with open(budget_path, "r") as budget_csv:
+        # loops over csv and converts lines into dictionaries
+        content = csv.reader(budget_csv)
+        limits = []
+        next(content)
+        for line in content:
+            limits.append(line[0], line[1], line[2], line[3], line[4])
+        return limits
 class Graph:
     def __init__(self, pieces, labels,title):
         self.pieces = pieces
@@ -62,10 +108,12 @@ def visualization_menu(user):
     spent_button = ctk.CTkButton(app,text="Create Pie Chart for Expenses By Category",command=spent_command,fg_color = "blue")
     
     
-    graph_button.pack()
-    pie_button.pack()
+    budget_button.pack()
+    spent_button.pack()
 
 
     app.mainloop()
+
+visualization_menu("Test")
 
 
